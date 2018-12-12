4bfeb92328a2c08bdd778ad0c9118783b374e860
Michael Irwin <mike.irwin1@gmail.com>
2018-12-12 15:48:59 +0000

    edit and update complete

--- students_controller.rb
+++ students_controller.rb
@@ -9,14 +9,31 @@ get '/students' do
   @students = Student.all()
   erb (:index)
 end
-# show
 
 # new
-
+get '/students/new' do
+  erb (:new)
+end
 # create
-
+post '/students' do
+  @student = Student.new( params )
+  @student.save
+  erb(:create)
+end
 # edit
-
+get '/students/:id/edit' do
+  @student = Student.find( params[:id] )
+  erb (:edit)
+end
 # update
-
+post '/students/:id' do
+  Student.new( params ).update
+  redirect to '/students'
+end
 # destroy
+
+# show
+get '/students/:id' do
+  @student = Student.find( params[:id] )
+  erb( :show )
+end
